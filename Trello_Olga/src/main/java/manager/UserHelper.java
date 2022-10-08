c8a8aa611578985c98047f8b1e6f4eb88cb33c2f
olacola06 <olamarchen@gmail.com>
2022-10-08 19:59:02 +0300

    08 Oct

--- Trello_Olga/src/main/java/manager/UserHelper.java
+++ Trello_Olga/src/main/java/manager/UserHelper.java
@@ -4,9 +4,15 @@ import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 public class UserHelper extends HelperBase{
     public UserHelper(WebDriver driver) {
@@ -70,17 +76,50 @@ public class UserHelper extends HelperBase{
         System.out.println("Recieved error message:-> "+message.toString());
         return true;
     }
-
-    public void initChangeAvatar() {
-        new WebDriverWait(wd, 15).until(ExpectedConditions.visibilityOf(
-                wd.findElement(By.xpath("//button[@data-test-id='header-member-menu-button']"))));
-        click(By.xpath("(//a[@href='/w/qa_12_olga/account'])[2]"));
-        click(By.xpath("(//button[@data-test-id='profileImage'])[1]"));
-        click(By.cssSelector("div[data-test-id='editAvatarPopover']"));
+    public void login(User user){
+        initLogin();
+        fillRegistForm(user);
+        submitLogin();
     }
 
-    public void uploadNewLogo() {
-        WebElement el = wd.findElement(By.cssSelector("div[data-test-id='editAvatarPopover']"));
-        el.sendKeys("C:/Users/Olga/Trello_Olga/Trello_Olga/src/test/resources/qa.png");
+    public void clickAvatar() {
+        new WebDriverWait(wd, 15).until(ExpectedConditions.visibilityOf(
+                wd.findElement(By.xpath("//button[@data-test-id='header-member-menu-button']"))));
+        click(By.xpath("//button[@data-test-id='header-member-menu-button']"));
+    }
+
+    public void openProfileAndVisibility() {
+        click(By.cssSelector("a[data-test-id='header-member-menu-profile']"));
+    }
+
+    public void openAtlassianProfile() {
+        click(By.cssSelector("a[href='https://id.atlassian.com/manage-profile']"));
+        System.out.println(wd.getWindowHandles());
+        List<String> tabs = new ArrayList<>(wd.getWindowHandles());
+        wd.switchTo().window(tabs.get(1));
+    }
+
+    public String getUrl() {
+        return wd.getCurrentUrl();
+    }
+
+    public String getTitlePage() {
+        return wd.getTitle();
+    }
+
+    public boolean getEmblemPresentOnPage() {
+        return wd.findElements(By.cssSelector("a[href='/manage-profile']")).size() > 0;
+    }
+
+    public void initChangePhoto() {
+        click(By.cssSelector("div[data-test-selector='profile-hover-info']"));
+        click(By.xpath("(//span[.='Add profile photo'])[4]"));
+    }
+
+    public void uploadPhoto(String link) {
+//        WebElement el = wd.findElement(By.xpath("//span[.='Upload a photo']"));//WRONG - need Input and Not Span
+//        el.sendKeys(link);
+        wd.findElement(By.id("image-input")).sendKeys(link);
+        click(By.xpath("//span[.='Upload']"));
     }
 }
--- Trello_Olga/src/test/java/ChangeAvatarTests.java
+++ Trello_Olga/src/test/java/ChangeAvatarTests.java
@@ -1,4 +1,5 @@
 import models.User;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -6,16 +7,22 @@ public class ChangeAvatarTests extends TestBase{
     @BeforeMethod
     public void preCondition(){
         if(!app.getUser().loginSuccess()){
-            app.getUser().initLogin();
-            app.getUser().fillRegistForm(User.builder()
+            app.getUser().login(User.builder()
                     .email("olamarchen@gmail.com").password("olacola06").build());
-            app.getUser().submitLogin();
+            logger.info("Test starts with user details:-> "+"Email: olamarchen@gmail.com, Password: olacola06");
         }
     }
     @Test
-    public void changeAvatar(){
-        app.getUser().initChangeAvatar();
-        app.getUser().uploadNewLogo();
+    public void changeAvatarSuccess(){
+        app.getUser().clickAvatar();
+        app.getUser().openProfileAndVisibility();
+        app.getUser().openAtlassianProfile();
+        Assert.assertTrue(app.getUser().getUrl().contains("id.atlassian.com"));
+        Assert.assertTrue(app.getUser().getTitlePage().contains("Atlassian account"));
+        //Assert.assertTrue(app.getUser().getEmblemPresentOnPage());
+
+        app.getUser().initChangePhoto();
+        app.getUser().uploadPhoto("C:/Users/Olga/Trello_Olga/Trello_Olga/src/test/resources/qa.png");
 
 
     }
